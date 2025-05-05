0a1a087ce860085036adb485ec4f03763c6171eb
Somsubhra Kundu <somsubhrak23@gmail.com>
2025-05-05 19:59:41 +0530

    UpdateSortJobSequencing.cpp
    bubble sort

--- JobSequencing.cpp
+++ JobSequencing.cpp
@@ -12,8 +12,8 @@ void sort(int n)
 {
 
     int i, j;
-    for (i = 1; i <= n; i++) 
-        for (j = 1; j <= n; j++) 
+    for (i = 1; i <= n-1; i++) 
+        for (j = 1; j <= n-1; j++) 
             if (p[j + 1] > p[j]) 
             {
                 swap(&p[j], &p[j + 1]);
